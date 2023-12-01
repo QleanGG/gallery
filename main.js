36bd61198d566f6835fc1ceb5db081992c2b7fb9
QleanGG <guyguz1@gmail.com>
2023-12-01 13:15:48 +0200

    added functions to show the artists and paintings picker, also function to choose paintings

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+// importing the artists from the other module
+import artists from "./modules/artists.js";
 
+const paintingsContainer = document.getElementById('paintingsContainer');
+
+function generateCheckbox() {
+    Object.keys(artists).forEach(artistKey => {
+        const artist = artists[artistKey];
+        
+        // artist name
+        paintingsContainer.innerHTML += `<h2>${artist.name}</h2>`;
+
+        // add painting names
+        artist.paintings.forEach((painting, index) => {
+            paintingsContainer.innerHTML += `
+            <label>
+                <input type="checkbox" name="${artistKey}_painting${index +1}" value="${painting.title}">
+                ${painting.title}
+                </label>
+                <br>
+            `;
+        });
+    });
+}
+
+function showcasePaintings() {
+    // get the checkboxes information
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    const selectedPaintings = Array.from(checkboxes).map(checkbox => checkbox.value);
+    
+    // set painting limit
+    const maxPaintings = 9;
+
+    const finalSelection = selectedPaintings.splice(0, maxPaintings);
+
+    // test
+    console.log('Selected Paintings:', finalSelection);
+
+    
+}
+
+generateCheckbox();
+
+
+document.getElementById('showButton').addEventListener('click',showcasePaintings);
--- modules/artists.js
+++ modules/artists.js
@@ -143,3 +143,5 @@ const artists = {
         ]
     },
 };
+
+export default artists; 
